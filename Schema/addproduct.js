9cb75e639593dfa90e37373aad5a187554717290
Satyapal Yadav <114231518+yadavsatyapal1999@users.noreply.github.com>
2024-04-14 23:05:45 +0530

    Update addproduct.js

--- Schema/addproduct.js
+++ Schema/addproduct.js
@@ -4,7 +4,7 @@ const addProduct =new mongoose.Schema({
     name:{type:String,required:true},
     price:{type:String,required:true},
     details:{type:String,required:true},
-   /* image:{type:String,required:true},*/
+    image:{type:String,required:true},
     stock:{type:Number,required:true},
     category:{type:String,required:true},
 })
