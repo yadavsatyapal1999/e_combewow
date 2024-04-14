12b82ad9e143fd4a7e77702b703ac7f086fafda0
Satyapal Yadav <114231518+yadavsatyapal1999@users.noreply.github.com>
2024-04-14 09:32:10 +0000

    CRUD implementation ready for testing

---
+++ Schema/addproduct.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const addProduct =new mongoose.Schema({
+    name:{type:String,required:true},
+    price:{type:String,required:true},
+    details:{type:String,required:true},
+    image:{type:String,required:true},
+    stock:{type:Number,required:true},
+    category:{type:String,required:true},
+})
+
+//userSchema.index({email:1}),{unique :true}) ;
+const Products = mongoose.model("Products",addProduct);
+
+module.exports = Products;
